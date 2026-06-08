c1cec9800704652d69d72f1e845f2b25a620e70b
ChanceRegine <chanceregineabijuru.com>
2026-06-08 13:33:34 +0200

    new changes

--- src/utils/validators.js
+++ src/utils/validators.js
@@ -5,17 +5,5 @@ export function validateWordInput(value) {
     return 'Please enter a word to search.';
   }
 
-  if (/\s+/.test(trimmed)) {
-    return 'Please search for one word, not a sentence.';
-  }
-
-  if (/\d/.test(trimmed)) {
-    return 'Please search for a word instead of numbers.';
-  }
-
-  if (!/^[a-zA-Z]+$/.test(trimmed)) {
-    return 'Please search for a word instead of symbols.';
-  }
-
   return '';
 }
